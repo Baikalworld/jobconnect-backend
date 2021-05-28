c07fca4ab11a476f0e9844da06f462ca01123fe0
Baikalworld <jan.vandinther11@gail.com>
2021-05-28 18:45:21 +0200

    no rainbow

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ require('colors');
 const express = require('express');
 //const cors = require('cors');
 
-const jobadds = require('./api/jobadds');
+//const jobadds = require('./api/jobadds');
 const errorHandler = require('./middlewares/error')
-const connectDB = require('./dbinit');
+//const connectDB = require('./dbinit');
 
 const app = express();
-connectDB();
+//connectDB();
 //app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => res.send('hello!!!'))
-app.use('/jobadds', jobadds)
+//app.use('/jobadds', jobadds)
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'.rainbow.bold.inverse))
+app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'))
