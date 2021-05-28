ba9f4a56974a8c096192b8711cf4038f6fa1c8c2
Baikalworld <jan.vandinther11@gail.com>
2021-05-28 18:40:44 +0200

    very last try

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
-
-
-/*require('dotenv').config();
+require('dotenv').config();
 require('colors');
 
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 
 const jobadds = require('./api/jobadds');
 const errorHandler = require('./middlewares/error')
@@ -26,10 +10,10 @@ const connectDB = require('./dbinit');
 
 const app = express();
 connectDB();
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => res.send('hello!!!'))
 app.use('/jobadds', jobadds)
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'.rainbow.bold.inverse))*/
+app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'.rainbow.bold.inverse))
