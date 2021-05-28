60bafc24b5e22a6a3ed3258fed3bc827536732c2
Baikalworld <jan.vandinther11@gail.com>
2021-05-28 18:36:03 +0200

    last try

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
+/*require('dotenv').config();
 require('colors');
 
 const express = require('express');
@@ -16,4 +32,4 @@ app.get('/', (req, res) => res.send('hello!!!'))
 app.use('/jobadds', jobadds)
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'.rainbow.bold.inverse))
+app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'.rainbow.bold.inverse))*/
