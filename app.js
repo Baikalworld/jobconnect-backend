8cf3437457c943ad92ee935ec6a531552e643165
Baikalworld <jan.vandinther11@gail.com>
2021-05-30 16:04:32 +0200

    change app.js line 16 on and 7 off

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('colors');
 const express = require('express');
 const cors = require('cors');
 
-const jobadds = require('./api/jobadds');
+// const jobadds = require('./api/jobadds');
 const errorHandler = require('./middlewares/error')
 const connectDB = require('./dbinit');
 
@@ -13,7 +13,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => res.send('hello!!!'))
-// app.use('/jobadds', jobadds)
+app.use('/jobadds', jobadds)
 app.use(errorHandler);
 
 app.listen(process.env.PORT || 5000, () => console.log('Started up server on http://localhost:5000'))
