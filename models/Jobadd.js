9e687b2534542f6af4647e6bc086dec55cfb10d8
Baikalworld <jan.vandinther11@gail.com>
2021-05-30 16:29:12 +0200

    change comment+-

--- models/Jobadd.js
+++ models/Jobadd.js
@@ -2,26 +2,26 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const JobaddSchema = new Schema({
-  addTitle: {
-    type: String,
-    required: [true, 'Please add a title'],
-    maxlength: [250, 'The title can be max 250 chars long']
-  },
-  addDate: {
-    type: Date,
-  },
-  addLocation: {
-    type: String,
-    required: [true, 'Please add a location']
-  },
-  addDesc: {
-    type: String,
-    required: [true, 'Please add a description']
-  },
-  addFull: {
-    type: [String],
-    required: [true, 'Please add a description']
-  },
+  // addTitle: {
+  //   type: String,
+  //   required: [true, 'Please add a title'],
+  //   maxlength: [250, 'The title can be max 250 chars long']
+  // },
+  // addDate: {
+  //   type: Date,
+  // },
+  // addLocation: {
+  //   type: String,
+  //   required: [true, 'Please add a location']
+  // },
+  // addDesc: {
+  //   type: String,
+  //   required: [true, 'Please add a description']
+  // },
+  // addFull: {
+  //   type: [String],
+  //   required: [true, 'Please add a description']
+  // },
   
   // addWrkt: {
   //   type: Schema.ObjectId,
