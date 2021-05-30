ff5d34e70c4c019801dcaa310c1174fd71083fb0
Baikalworld <jan.vandinther11@gail.com>
2021-05-30 16:34:36 +0200

    change comment+-

--- API/jobadds.js
+++ API/jobadds.js
@@ -1,4 +1,4 @@
-const express = require("express");
+// const express = require("express");
 
 // const {
 //   getJobadd,
@@ -21,4 +21,4 @@ const express = require("express");
 //   .delete(deleteJobadd)
 //   .put(updateJobadd);
 
-module.exports = api;
+// module.exports = api;
